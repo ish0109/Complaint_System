f098ccd9deac9908507683c6dbd5f63d6025c486
Ishita Ghosh <ishitag0109@gmail.com>
2025-06-16 11:53:51 +0530

    Fix: Added Render domain to ALLOWED_HOSTS

--- complaint_system/settings.py
+++ complaint_system/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!l8ml!bxbm$hn=_0#^iee!mzaj3r5e%g(n4=m*sfdh!m&e3yg2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['complaint-system-y2cf.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
